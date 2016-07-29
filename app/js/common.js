99d71a4db327267cee17539589b6b29ed78b3857
Anatoliy <cata07@yandex.ru>
2016-07-29 22:28:24 +0300

    Сверстал sidebar

--- app/js/common.js
+++ app/js/common.js
@@ -5,5 +5,15 @@ $(function() {
 
   $(".search").on("click", function(){
     $(".header__bottom .navbar-form").slideToggle();
-  })
+  });
 });
+
+// $(window).load(function(){
+//   var carouselCaptionWidth = $("#carousel-sidebar .active img").width();
+//   $("#carousel-sidebar img").each(function(){
+//     $(this).attr("width", carouselCaptionWidth);
+//   });
+//   $("#carousel-sidebar img").each(function(){
+//     $("#carousel-sidebar .sidebar-carousel-caption").css("max-width", carouselCaptionWidth + "px");
+//   });
+// });
